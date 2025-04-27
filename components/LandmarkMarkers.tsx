2ed488c982495022018e3cddad43ee7965521be1
zneright2 <buday.313258@caloocan.sti.edu.ph>
2025-04-27 23:14:29 +0800

    Fix camera opening issue: added device and isActive props to Camera component

--- components/LandmarkMarkers.tsx
+++ components/LandmarkMarkers.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable */
 import { CircleLayer, Images, ShapeSource, SymbolLayer } from "@rnmapbox/maps";
 import { OnPressEvent } from "@rnmapbox/maps/lib/typescript/src/types/OnPressEvent";
-import pin from '../assets/pinB.png'
+import pin from '../assets/pinB.png';
 import landmarks from '../data/landmark.json';
 import { featureCollection, point } from '@turf/helpers';
 
 import { useLandmark } from "../provider/LandmarkProvider";
-export default function LandmarkMarkers(){
-    const { setSelectedLandmark} = useLandmark();
+export default function LandmarkMarkers() {
+    const { setSelectedLandmark } = useLandmark();
     const points = landmarks.map((landmark) => point([landmark.longitude, landmark.latitude], { landmark }));
     const onPointPress = async (event: OnPressEvent) => {
         if (event.features[0].properties?.landmark) {
@@ -16,7 +16,7 @@ export default function LandmarkMarkers(){
 
 
     };
-    return(
+    return (
         <ShapeSource id="landmarks" shape={featureCollection(points)}
             onPress={onPointPress}>
 
@@ -27,7 +27,7 @@ export default function LandmarkMarkers(){
                     textPitchAlignment: 'map',
                 }} />
 
-            
+
             <SymbolLayer id="landmark-icons"
                 filter={['!', ['has', 'point_count']]}
                 style={{
